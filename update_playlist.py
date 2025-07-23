2147d1e5ae26b4317cf4cf5c68ca2e62551c915f
askishq <free16697@gmail.com>
2025-07-23 07:00:20 +0600

    Update update_playlist.py

--- update_playlist.py
+++ update_playlist.py
@@ -1,15 +1,1 @@
-import requests
 
-def fetch_latest_playlist():
-    url = "https://iptv-org.github.io/iptv/countries/bd.m3u"  # আপনার সোর্স লিংক
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open("playlist.m3u", "w", encoding="utf-8") as f:
-            f.write(response.text)
-        print("Playlist updated successfully.")
-    else:
-        print("Failed to fetch playlist")
-
-if __name__ == "__main__":
-    fetch_latest_playlist()
-  
